97c62a4eafde3e1d86503c7bd0c6d09d6d3be35a
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-08-01 14:43:48 -0600

    Update database.c

--- software/esp/hello_world/main/database.c
+++ software/esp/hello_world/main/database.c
@@ -7,6 +7,18 @@
 #define FIREBASE_REST_USERS "/users"
 #define FIREBASE_REST_REMINDERS "/reminders"
 
+#define REMINDERS_BASE_LENGTH 16
+
+typedef enum direction_e{
+    COMING,
+    GOING
+}direction_t;
+
+typedef struct reminder{
+    char* user_id;
+    direction_t direction;
+}reminder_t;
+
 static esp_http_client_handle_t firebase_rest_handle;
 
 //creates handle to https client with Firebase REST API url
@@ -40,11 +52,25 @@ void get_device_users()
 void get_user_reminders(char* user_id)
 {
     char* device_users_url;
-    sprintf(device_users_url, 
-            FIREBASE_REST_BASE,
-            FIREBASE_REST_DEVICES,
-            '/', singleton_get_device_id(),
-            FIREBASE_REST_REMINDERS
-    );
+        sprintf(device_users_url, 
+                FIREBASE_REST_BASE,
+                FIREBASE_REST_DEVICES,
+                '/', singleton_get_device_id(),
+                FIREBASE_REST_REMINDERS
+        );
     esp_http_client_set_url(firebase_rest_handle, device_users_url);
+    reminder_t reminders[REMINDERS_BASE_LENGTH];
+    size_t reminders_size = 0;
+    //get/parse reminders from firebase
+
+    user_array_t *users = singleton_get_users();
+    for (size_t i = 0; i < reminders_size; i++)
+    {
+        for (size_t i = 0; i < users->length; i++)
+        {
+            //check each reminder to see who's it is.
+            //if user ids match, set corresponding flag
+            //for user (coming, going) in singleton
+        }
+    }
 }
