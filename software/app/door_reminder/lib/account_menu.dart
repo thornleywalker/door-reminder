b7f6e1ccdd89e5fbdf2ae03d81480e3e395a43cf
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-08-04 14:51:02 -0600

    added bodies to menu generators

---
+++ software/app/door_reminder/lib/account_menu.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:door_reminder/screens/account.dart';
+import 'package:door_reminder/login_services/authentication.dart';
+import 'package:door_reminder/singleton.dart';
+
+class AccountMenu extends StatelessWidget {
+  AccountMenu();
+  static var mySingleton = Singleton();
+  var context;
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return IconButton(
+        icon: Icon(Icons.account_circle),
+        iconSize: 45,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AccountRoute()),
+          );
+        });
+  }
+}
---
+++ software/app/door_reminder/lib/hamburger_menu.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:door_reminder/screens/account.dart';
+import 'package:door_reminder/screens/settings.dart';
+import 'package:door_reminder/screens/help.dart';
+import 'package:door_reminder/singleton.dart';
+
+class HamburgerMenu extends StatelessWidget {
+  HamburgerMenu();
+
+  static var mySingleton = Singleton();
+  static String userName, userEmail;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: <Widget>[
+        UserAccountsDrawerHeader(
+          accountName: FutureBuilder(
+              future: mySingleton.userID(),
+              builder: (context, AsyncSnapshot<String> uid) {
+                if (uid.hasData) return Text('UserID: ' + uid.data);
+              }),
+          accountEmail: FutureBuilder(
+              future: mySingleton.email(),
+              builder: (context, AsyncSnapshot<String> email) {
+                if (email.hasData) return Text('UserID: ' + email.data);
+              }),
+          currentAccountPicture: CircleAvatar(
+            backgroundColor: Theme.of(context).platform == TargetPlatform.iOS
+                ? Colors.blue
+                : Colors.white,
+            child: FutureBuilder(
+                future: mySingleton.email(),
+                builder: (context, AsyncSnapshot<String> email) {
+                  if (email.hasData)
+                    return Text(
+                      email.data.substring(0, 1).toUpperCase(),
+                      style: TextStyle(fontSize: 40.0),
+                    );
+                }),
+          ),
+        ),
+        ListTile(
+          title: Text("Home"),
+          onTap: () {
+            while (Navigator.canPop(context)) {
+              Navigator.pop(context);
+            }
+          },
+        ),
+        ListTile(
+          title: Text("Account"),
+          onTap: () {
+            Navigator.pop(context); //close drawer
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AccountRoute()),
+            );
+          },
+        ),
+        ListTile(
+          title: Text("Settings"),
+          onTap: () {
+            Navigator.pop(context); //close drawer
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SettingsRoute()),
+            );
+          },
+        ),
+        ListTile(
+          title: Text("Help"),
+          onTap: () {
+            Navigator.pop(context); //close drawer
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HelpRoute()),
+            );
+          },
+        ),
+      ]),
+    );
+  }
+}
