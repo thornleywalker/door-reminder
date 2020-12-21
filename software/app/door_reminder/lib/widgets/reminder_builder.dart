874f931099e8c7f16b514b46bcac0a286f7c2796
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-21 10:19:12 -0700

    added onComplete callback

--- software/app/door_reminder/lib/widgets/reminder_builder.dart
+++ software/app/door_reminder/lib/widgets/reminder_builder.dart
@@ -3,6 +3,9 @@ import 'package:door_reminder/services/data_cache.dart';
 import 'package:flutter/material.dart';
 
 class ReminderBuilder extends StatefulWidget {
+  ReminderBuilder({this.onComplete});
+
+  Function onComplete;
   @override
   State<StatefulWidget> createState() {
     return _ReminderBuilder();
@@ -10,99 +13,111 @@ class ReminderBuilder extends StatefulWidget {
 }
 
 class _ReminderBuilder extends State<ReminderBuilder> {
-  Reminder reminder = Reminder();
+  DataCache dc = DataCache();
 
-  String description, body, destination;
-  bool direction;
+  TextEditingController desc = TextEditingController(),
+      body = TextEditingController(),
+      dest = TextEditingController(),
+      dir = TextEditingController();
+
+  // String description, body, destination;
+  // bool direction;
 
   final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    dir.text = "Going";
     return AlertDialog(
-        contentPadding: EdgeInsets.all(10),
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Description", helperText: "Description"),
-                      validator: (value) {
-                        if (value.isEmpty)
-                          return "Description can't be left empty";
-                        return null;
-                      },
-                      onSaved: (val) => reminder.description = val,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      decoration:
-                          InputDecoration(hintText: "Body", helperText: "Body"),
-                      validator: (value) {
-                        return null;
-                      },
-                      onSaved: (val) => reminder.body = val,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Destination", helperText: "Destination"),
-                      validator: (value) {
-                        return null;
-                      },
-                      onSaved: (val) => reminder.description = val,
-                    ),
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(
-                          hintText: "Select direction",
-                          helperText: "Direction"),
-                      items: ["Going", "Coming"].map((val) {
-                        return DropdownMenuItem(
-                          value: val,
-                          child: Text(val),
-                        );
-                      }).toList(),
-                      onChanged: (val) =>
-                          reminder.direction = val.toLowerCase(),
-                      validator: (val) {
-                        if (reminder.direction == null)
-                          return "Direction required";
-                        return null;
-                      },
-                      onSaved: (val) => reminder.direction = val.toLowerCase(),
-                    )
-                  ],
-                ),
-              ),
-              Row(
+      contentPadding: EdgeInsets.all(10),
+      content: SingleChildScrollView(
+        child: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
                 children: [
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.pop(context);
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Description", helperText: "Description"),
+                    validator: (value) {
+                      if (value.isEmpty)
+                        return "Description can't be left empty";
+                      return null;
                     },
-                    child: Text("Cancel"),
+                    controller: desc,
                   ),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState.validate()) {
-                          print("form is valid");
-                          formKey.currentState.save();
-                          reminder.uid = 'testid';
-                          reminder.did = 'testdevice';
-                          DataCache().addReminder(reminder);
-                          Navigator.pop(context);
-                        } else
-                          print("form invalid");
-                      },
-                      child: Text("Create"))
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration:
+                        InputDecoration(hintText: "Body", helperText: "Body"),
+                    validator: (value) {
+                      return null;
+                    },
+                    controller: body,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Destination", helperText: "Destination"),
+                    validator: (value) {
+                      return null;
+                    },
+                    controller: dest,
+                  ),
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                        hintText: "Select direction", helperText: "Direction"),
+                    items: ["Going", "Coming"].map((val) {
+                      return DropdownMenuItem(
+                        value: val,
+                        child: Text(val),
+                      );
+                    }).toList(),
+                    onChanged: (val) => dir.text = val,
+                    value: dir.text,
+                    validator: (val) {
+                      if (val.isEmpty) return "Direction required";
+                      return null;
+                    },
+                    onSaved: (val) => dir.text = val.toLowerCase(),
+                  )
                 ],
               ),
-            ],
-          ),
-        ));
+            ),
+            Row(
+              children: [
+                FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState.validate()) {
+                      print("form is valid");
+                      dc.addReminder(
+                        Reminder(
+                            description: desc.text,
+                            body: body.text,
+                            destination: dest.text,
+                            direction: dir.text,
+                            uid: dc.getUserID(),
+                            did: 'deviceid'),
+                      );
+                      Navigator.pop(context);
+                      widget.onComplete();
+                    } else
+                      print("form invalid");
+                  },
+                  child: Text("Create"),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
