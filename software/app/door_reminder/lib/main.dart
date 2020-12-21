c6f9eee912fa3083461cf56e6f7987ea7204b0ae
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-21 10:17:33 -0700

    rearranged file structure

--- software/app/door_reminder/lib/main.dart
+++ software/app/door_reminder/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:door_reminder/services/login_page.dart';
+import 'package:door_reminder/screens/login_page.dart';
 import 'package:door_reminder/services/authentication.dart';
 
 Future<void> main() async {
