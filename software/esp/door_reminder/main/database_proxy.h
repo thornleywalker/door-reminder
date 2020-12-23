8df58ce64e25f36f97d3c142b43ad4fde0818435
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-22 19:26:12 -0700

    added init function

--- software/esp/door_reminder/main/database_proxy.h
+++ software/esp/door_reminder/main/database_proxy.h
@@ -1,11 +1,12 @@
 #ifndef DATABASE_PROXY_H
 #define DATABASE_PROXY_H
 
+#include "esp_err.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+esp_err_t database_init();
 void database_login();
 void database_sync();
-void testing_function();
 
 #endif
