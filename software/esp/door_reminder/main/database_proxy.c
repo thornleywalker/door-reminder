36c88e059566dd8bbd9e85bc136d856941fc83bb
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-23 15:23:16 -0700

    fixed json parsing and flag setting

--- software/esp/door_reminder/main/database_proxy.c
+++ software/esp/door_reminder/main/database_proxy.c
@@ -3,6 +3,7 @@
 #include "data_cache.h"
 #include "esp_http_client.h"
 #include "esp_log.h"
+#include <ctype.h>
 #include <string.h>
 
 #define FIREBASE_REST_BASE "https://door-reminder-89e84.firebaseio.com"
@@ -14,7 +15,7 @@
 #define REMINDERS_BASE_LENGTH 16
 #define MAX_HTTP_OUTPUT_BUFFER 2048
 
-static const char *TAG = "database_proxy";
+static const char *TAG = "dr_database_proxy";
 
 typedef enum direction_e { COMING, GOING } direction_t;
 
@@ -67,27 +68,28 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
     break;
   case HTTP_EVENT_ON_FINISH:
     ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-    if (output_buffer != NULL) {
-      // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated
-      // response ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
-      free(output_buffer);
-      output_buffer = NULL;
-    }
-    output_len = 0;
+    // if (output_buffer != NULL) {
+    //   // Response is accumulated in output_buffer. Uncomment the below line to print the
+    //   accumulated
+    //   // response ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+    //   free(output_buffer);
+    //   output_buffer = NULL;
+    // }
+    // output_len = 0;
     break;
   case HTTP_EVENT_DISCONNECTED:
     ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
-    int mbedtls_err = 0;
-    esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
-    if (err != 0) {
-      if (output_buffer != NULL) {
-        free(output_buffer);
-        output_buffer = NULL;
-      }
-      output_len = 0;
-      ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
-      ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
-    }
+    // int mbedtls_err = 0;
+    // esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+    // if (err != 0) {
+    //   if (output_buffer != NULL) {
+    //     free(output_buffer);
+    //     output_buffer = NULL;
+    //   }
+    //   output_len = 0;
+    //   ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+    //   ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+    // }
     break;
   }
   return ESP_OK;
@@ -116,104 +118,76 @@ char *json_parse_user(char *str, user_t *user) {
   const char dlim[] = "{}:\"\n\r, ";
   char *curr_token;
 
+  user->has_going_reminders = false;
+  user->has_coming_reminders = false;
+
   // parse user id
-  curr_token = strtok(str, dlim);
-  strcpy(curr_token, user->id);
+  curr_token = strtok(NULL, dlim);
+  if (curr_token == NULL)
+    return NULL;
+  strcpy(user->id, curr_token);
+  if (curr_token == NULL) {
+    printf("got null unexpectedly\n");
+    return NULL;
+  }
+  // printf("id token: %s\n", curr_token);
 
   // consume "coming"
-  strtok(NULL, dlim);
-  // consume coming "count"
-  strtok(NULL, dlim);
-  // parse coming count value
   curr_token = strtok(NULL, dlim);
-  if (atoi(curr_token) != 0)
-    user->has_coming_reminders = true;
-  else
-    user->has_coming_reminders = false;
-  // consume coming "flag"
-  strtok(NULL, dlim);
-  // consume coming flag value
-  strtok(NULL, dlim);
+  // printf("coming/going token: %s\n", curr_token);
+  if (strcmp(curr_token, "coming") == 0) {
+    // consume coming "count"
+    curr_token = strtok(NULL, dlim);
+    // printf("count token: %s\n", curr_token);
 
-  // consume "going"
-  strtok(NULL, dlim);
-  // consume going "count"
-  strtok(NULL, dlim);
-  // parse going count value
-  curr_token = strtok(NULL, dlim);
-  if (atoi(curr_token) != 0)
-    user->has_going_reminders = true;
-  else
-    user->has_going_reminders = false;
-  // consume going "flag"
-  strtok(NULL, dlim);
-  // consume going flag value
-  strtok(NULL, dlim);
+    // parse coming count value
+    curr_token = strtok(NULL, dlim);
+    // printf("count token: %s\n", curr_token);
 
-  return strtok(NULL, dlim);
-}
+    if (atoi(curr_token) != 0)
+      user->has_coming_reminders = true;
+    else
+      user->has_coming_reminders = false;
+    // consume coming "flag"
+    curr_token = strtok(NULL, dlim);
+    // printf("flag token: %s\n", curr_token);
 
-esp_err_t database_init() {
-  update_all_reminders();
-  ESP_LOGI(TAG, "database proxy initialized\n");
+    // consume coming flag value
+    curr_token = strtok(NULL, dlim);
+    // printf("flag value token: %s\n", curr_token);
 
-  return ESP_OK;
-}
+    // consume "going"
+    curr_token = strtok(NULL, dlim);
+    // printf("going token: %s\n", curr_token);
+  }
+  if (!strcmp(curr_token, "going")) {
+    // consume going "count"
+    curr_token = strtok(NULL, dlim);
+    // printf("count token: %s\n", curr_token);
 
-// gets users from Firebase, adds them to data_cache user array
-// returns true if successful
-bool get_device_users() {
-  char *device_users_url;
-  sprintf(device_users_url, "%s%s%s%s%s" FIREBASE_REST_BASE, FIREBASE_REST_DEVICES, "/",
-          data_cache_get_device_id(), FIREBASE_REST_SUFFIX);
-  esp_err_t err = esp_http_client_set_url(firebase_rest_handle, device_users_url);
-
-  if (err != ESP_OK) {
-    ESP_LOGW(TAG, "could not get webpage\n");
-    return false;
-  } else {
-    printf("%s", );
+    // parse going count value
+    curr_token = strtok(NULL, dlim);
+    // printf("count token: %s\n", curr_token);
+    if (atoi(curr_token) != 0)
+      user->has_going_reminders = true;
+    else
+      user->has_going_reminders = false;
+    // consume going "flag"
+    curr_token = strtok(NULL, dlim);
+    // printf("flag token: %s\n", curr_token);
+    // consume going flag value
+    curr_token = strtok(NULL, dlim);
+    // printf("flag value token: %s\n", curr_token);
   }
 
-  // GET json object
-  char *json_string;
-
-  // parse values
-  char *curr_string = json_string;
-  for (int i = 0; i < MAX_USERS; i++)
-    if (json_parse_user(curr_string, data_cache_get_user(i)) == NULL)
-      break;
-
-  return true;
-}
-
-// checks for reminders for each user in data_cache
-void get_user_reminders(char *user_id) {
-  char *device_users_url;
-  // turn this into a macro
-  sprintf(device_users_url, FIREBASE_REST_BASE, FIREBASE_REST_DEVICES, '/',
-          data_cache_get_device_id(), FIREBASE_REST_REMINDERS);
-  esp_http_client_set_url(firebase_rest_handle, device_users_url);
-  reminder_t reminders[REMINDERS_BASE_LENGTH];
-  size_t reminders_size = 0;
-  // get/parse reminders from firebase
-
-  user_array_t *users = data_cache_get_users();
-  for (size_t i = 0; i < reminders_size; i++) {
-    for (size_t i = 0; i < users->length; i++) {
-      // check each reminder to see who's it is.
-      // if user ids match, set corresponding flag
-      // for user (coming, going) in data_cache
-    }
-  }
+  return strtok(NULL, "\"");
 }
 
 void update_all_reminders() {
+  ESP_LOGI(TAG, "updating reminders");
   char device_users_url[512];
-
-  sprintf(device_users_url, FIREBASE_REST_BASE, FIREBASE_REST_DEVICES, '/',
+  sprintf(device_users_url, "%s%s%s%s%s", FIREBASE_REST_BASE, FIREBASE_REST_DEVICES, "/",
           data_cache_get_device_id(), FIREBASE_REST_SUFFIX);
-
   esp_http_client_config_t config = {
       .url = device_users_url,
       .method = HTTP_METHOD_GET,
@@ -223,11 +197,96 @@ void update_all_reminders() {
   };
 
   // setup client
-  firebase_rest_handle = esp_http_client_init(&firebase_configuration);
+  firebase_rest_handle = esp_http_client_init(&config);
   // perform get
   esp_http_client_perform(firebase_rest_handle);
   // check response
-  printf("\n\n\n%s\n\n\n", response_buffer);
+  printf("\n\n%s\n\n", response_buffer);
+
+  char *buf_loc = response_buffer;
+  user_t *temp_user;
+  int index = 0;
+  data_cache_get_users()->length = 0;
+  temp_user = data_cache_get_user(index);
+  // load response into strtok internal buffer
+  buf_loc = strtok(response_buffer, "\"");
+  while ((buf_loc = json_parse_user(buf_loc, temp_user)) != NULL) {
+    printf("id: %s\ngoing?: %i\ncoming?: %i\n", temp_user->id, temp_user->has_going_reminders,
+           temp_user->has_coming_reminders);
+    temp_user = data_cache_get_user(++index);
+    data_cache_get_users()->length++;
+  }
+
+  ESP_LOGI(TAG, "all device info parsed");
+
   // cleanup
   esp_http_client_cleanup(firebase_rest_handle);
+  ESP_LOGI(TAG, "reminders updated");
+}
+
+static bool is_initialized = false;
+esp_err_t database_init() {
+  // avoid reinitialization
+  if (is_initialized)
+    return ESP_OK;
+
+  ESP_LOGI(TAG, "initializing database proxy");
+  update_all_reminders();
+  ESP_LOGI(TAG, "database proxy initialized");
+  is_initialized = true;
+  return ESP_OK;
+}
+
+// sets corresponding flag in database
+void set_reminder_flag(char *user_id, alert_dir_t direction) {
+  char *dir_string;
+  if (direction == ALERT_DIR_COMING)
+    dir_string = "coming";
+  else if (direction == ALERT_DIR_GOING)
+    dir_string = "going";
+  else
+    return;
+
+  ESP_LOGI(TAG, "setting reminder flag. user: %s, direction: %s", user_id, dir_string);
+
+  char device_flag_url[512];
+  sprintf(device_flag_url, "%s%s%s%s%s%s%s%s%s", FIREBASE_REST_BASE, FIREBASE_REST_DEVICES, "/",
+          data_cache_get_device_id(), "/", user_id, "/", dir_string, FIREBASE_REST_SUFFIX);
+
+  esp_http_client_set_url(firebase_rest_handle, device_flag_url);
+  const char *post_data = "{\"flag\":true}";
+  esp_http_client_set_header(firebase_rest_handle, "Content-Type", "application/json");
+  esp_http_client_set_post_field(firebase_rest_handle, post_data, strlen(post_data));
+  // perform get
+  esp_http_client_perform(firebase_rest_handle);
+
+  ESP_LOGI(TAG, "reminder flag set");
+}
+
+// checks every user in the array for reminders, triggers them
+esp_err_t database_alert_users(alert_dir_t direction) {
+  ESP_LOGI(TAG, "alerting users");
+  user_array_t *users_array_h = data_cache_get_users();
+
+  esp_http_client_config_t config = {.url = FIREBASE_REST_BASE,
+                                     .method = HTTP_METHOD_PATCH,
+                                     .event_handler = _http_event_handler,
+                                     .user_data = response_buffer,
+                                     .disable_auto_redirect = true};
+  // setup client
+  firebase_rest_handle = esp_http_client_init(&config);
+
+  for (int i = 0; i < users_array_h->length; i++) {
+    if ((direction & ALERT_DIR_COMING) && users_array_h->users[i].has_coming_reminders)
+      set_reminder_flag(users_array_h->users[i].id, direction);
+
+    if ((direction & ALERT_DIR_GOING) && users_array_h->users[i].has_going_reminders)
+      set_reminder_flag(users_array_h->users[i].id, direction);
+  }
+
+  esp_http_client_cleanup(firebase_rest_handle);
+
+  ESP_LOGI(TAG, "users alerted");
+
+  return ESP_OK;
 }
--- software/esp/door_reminder/main/database_proxy.h
+++ software/esp/door_reminder/main/database_proxy.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-esp_err_t database_init();
-void database_login();
-void database_sync();
+typedef enum { ALERT_DIR_COMING = (1 << 0), ALERT_DIR_GOING = (1 << 1) } alert_dir_t;
 
+esp_err_t database_init();
+esp_err_t database_alert_users(alert_dir_t direction);
 #endif
