b363f0a157d22854564def569da1ba4b46b152dc
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-23 15:17:18 -0700

    changed tag to identify project logs

--- software/esp/door_reminder/main/bluetooth.c
+++ software/esp/door_reminder/main/bluetooth.c
@@ -2,7 +2,7 @@
 
 #include "esp_log.h"
 
-static const char *TAG = "bluetooth";
+static const char *TAG = "dr_bluetooth";
 
 esp_err_t bluetooth_init() {
   ESP_LOGI(TAG, "bluetooth initialized\n");
