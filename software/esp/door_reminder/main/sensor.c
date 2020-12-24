9f86c026b5f29da81572c5cbb597fbc7463151cc
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-12-24 12:44:00 -0700

    added separate task to handle notifications

--- software/esp/door_reminder/main/sensor.c
+++ software/esp/door_reminder/main/sensor.c
@@ -3,6 +3,9 @@
 #include "database_proxy.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <limits.h>
 
 static const char *TAG = "dr_sensor";
 
@@ -11,21 +14,39 @@ static const char *TAG = "dr_sensor";
 #define INPUT_PIN_SELECT ((1ULL << GOING_INPUT) | (1ULL << COMING_INPUT))
 #define ESP_INTR_FLAG_DEFAULT 0
 
+xTaskHandle button_task;
+
 void going_isr() {
-  // ESP_LOGI(TAG, "interrupt detected on 22\n");
-  database_alert_users(ALERT_DIR_GOING);
-  // ESP_LOGI(TAG, "interrupt executed\n");
+  // send alert direction to handler task
+  xTaskGenericNotifyFromISR(button_task, ALERT_DIR_GOING, eSetValueWithOverwrite, NULL, NULL);
 }
 
 void coming_isr() {
-  // ESP_LOGI(TAG, "interrupt detected on 2\n");
-  database_alert_users(ALERT_DIR_COMING);
-  // ESP_LOGI(TAG, "interrupt executed\n");
+  // send alert direction to handler task
+  xTaskGenericNotifyFromISR(button_task, ALERT_DIR_COMING, eSetValueWithOverwrite, NULL, NULL);
+}
+
+static void interrupt_handler(void *arg) {
+  alert_dir_t dir;
+
+  for (;;) {
+    if (xTaskNotifyWait(ULONG_MAX, ULONG_MAX, &dir, pdMS_TO_TICKS(1000)) == pdPASS) {
+      printf("direction: %s\n",
+             (dir == ALERT_DIR_COMING) ? "coming" : (dir == ALERT_DIR_GOING) ? "going" : "error");
+      database_alert_users(dir);
+      vTaskDelay(pdMS_TO_TICKS(1000));
+      printf("finished task sleep");
+      ESP_LOGI(TAG, "Finished task sleeping")
+    }
+  }
 }
 
 // sets up pin for input, registers isr
 esp_err_t sensor_init() {
   ESP_LOGI(TAG, "initializing sensor");
+
+  // setup button handling task
+  xTaskCreate(interrupt_handler, "interrupt_handler", 10240, NULL, 10, &button_task);
 
   gpio_config_t cnfg = {
       .intr_type = GPIO_INTR_POSEDGE,
